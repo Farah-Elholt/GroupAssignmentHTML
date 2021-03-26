07c5e9c5a857b279c9b14373c5a7c98ffc75648c
codysch8 <78003662+codysch8@users.noreply.github.com>
2021-03-26 16:36:59 -0600

    d done (number generator)

--- random.js
+++ random.js
@@ -24,6 +24,16 @@ function changeElement() {
       x.value = "";
       if (y.style.color = "white") {
           y.style.color = "black";
-      }
+      }     
+  }
 
+  function generateNum() {
+    var num1 = document.getElementById('numInput1').value;
+    var num2 = document.getElementById('numInput2').value;
+
+    min = Math.ceil(num1);
+    max = Math.floor(num2);
+    var result =  Math.ceil(Math.random() * (num2 - num1 + 1));
+
+    document.getElementById("resultLabel").innerHTML = result.toString();
   }
